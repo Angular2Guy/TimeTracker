10156d1c3e79775ff3f5ef6da4ef9f582deb0464
Angular2guy <angular2guy@gmail.com>
2026-01-17 16:35:56 +0100

    feat: redirect to login

--- frontend/app/login/login.tsx
+++ frontend/app/login/login.tsx
@@ -1,3 +1,14 @@
+import { useAtom } from "jotai";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import GlobalState from "~/global-state";
+
 export function Login() {
+  const navigate = useNavigate();  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [globalJwtTokenState, setGlobalJwtTokenState] = useAtom(GlobalState.jwtToken);
+  const [globalRolesState, setGlobalRolesState] = useAtom(GlobalState.roles);
+
   return (<div>This is the login page</div>);
 }
--- frontend/app/main/main.tsx
+++ frontend/app/main/main.tsx
@@ -1,6 +1,19 @@
+import { useAtom } from "jotai";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import GlobalState from "~/global-state";
 
 
 export function Main() {
+  const navigate = useNavigate();
+  const [globalJwtTokenState, setGlobalJwtTokenState] = useAtom(GlobalState.jwtToken);
+
+  useEffect(() => {
+    if (!globalJwtTokenState || globalJwtTokenState.length === 0) {
+      navigate('/login');
+    }    
+    }, [globalJwtTokenState]);
+
   return (<div>Main Page</div>
   );
 }
