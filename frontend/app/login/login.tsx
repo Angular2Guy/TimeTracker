0d0a67d0e50fc8fa0f5774c8950030dd6e998f91
Angular2guy <angular2guy@gmail.com>
2026-01-18 11:35:56 +0100

    feat: add login

--- frontend/app/login/login.tsx
+++ frontend/app/login/login.tsx
@@ -11,16 +11,215 @@
    limitations under the License.
  */
 import { useAtom } from "jotai";
-import { useEffect, useState } from "react";
+import { useEffect, useState, type BaseSyntheticEvent, type ChangeEventHandler, type FormEvent } from "react";
+import { Dialog, DialogContent, Button, Tabs, Tab, Box, TextField, type SelectChangeEvent, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import { useNavigate } from "react-router";
 import GlobalState from "~/global-state";
+import { useTranslation, Trans } from 'react-i18next';
+import styles from './login.module.css';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <>{children}</>
+      )}
+    </div>
+  );
+}
 
 export function Login() {
+  const { t, i18n } = useTranslation();
   const navigate = useNavigate();  
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [password1, setPassword1] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [activeTab, setActiveTab] = useState(0);  
+  const [language, setLanguage] = useState('en');
+  const [responseMsg, setResponseMsg] = useState('');
   const [globalJwtTokenState, setGlobalJwtTokenState] = useAtom(GlobalState.jwtToken);
   const [globalRolesState, setGlobalRolesState] = useAtom(GlobalState.roles);
 
-  return (<div>This is the login page</div>);
+  const handleChangeEmail: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setEmail(event.currentTarget.value as string);
+  };
+  const handleChangePassword1: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setPassword1(event.currentTarget.value as string);
+  };
+  const handleChangePassword2: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setPassword2(event.currentTarget.value as string);
+  };
+
+  const handleCancel = (event: FormEvent) => {
+    event.preventDefault();
+    setEmail('');
+    setPassword1('');
+    setPassword2('');
+    setResponseMsg('');
+  };
+  const handleClose = () => {
+    //setOpen(false);
+  };
+  const handleTabChange = (event: BaseSyntheticEvent, newValue: number) => {
+    setActiveTab(newValue);
+    setResponseMsg('');
+  };
+  const a11yProps = (index: number) => {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+
+  const handleLanguageChange = (event: SelectChangeEvent) => {
+    setLanguage(event.target.value);
+    i18n.changeLanguage(event.target.value).then();
+  };
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+    throw new Error("Function not implemented.");
+  }
+
+  return (<div className={styles.content}>
+    <Tabs value={activeTab} onChange={handleTabChange} aria-label="basic tabs example">
+        <Tab label={t('login.login')} {...a11yProps(0)} />
+        <Tab label={t('login.signin')} {...a11yProps(1)} />
+      </Tabs>
+      <TabPanel value={activeTab} index={0}>
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+          onSubmit={handleSubmit}
+        >
+          <TextField
+            autoFocus
+            margin="dense"
+            value={email}
+            onChange={handleChangeEmail}
+            id="email"
+            label={t('login.email')}
+            type="string"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            value={password1}
+            onChange={handleChangePassword1}
+            id="password1"
+            label={t('login.password')}
+            type="password"
+            fullWidth
+            variant="standard"
+          />
+          <div className="flex justify-between items-center mt-4">
+            <div>
+            <Button type="submit">{t('common.ok')}</Button>
+            <Button onClick={handleCancel}>{t('common.cancel')}</Button>
+            </div>
+            <FormControl sx={{ minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-small-label">{t('common.language')}</InputLabel>
+      <Select
+        labelId="demo-select-small-label"
+        id="demo-select-small"
+        value={language}
+        label={t('common.language')}
+        onChange={handleLanguageChange}
+      >
+        <MenuItem value={'en'}>{t('common.english')}</MenuItem>
+        <MenuItem value={'de'}>{t('common.german')}</MenuItem>
+      </Select>
+    </FormControl>
+          </div>          
+          <div className={styles.responseMsg}>
+            {[responseMsg].filter(value => !!value).map((value, index) =>
+              <span key={index}>{t('common.message')}: {value}</span>
+            )}
+          </div>
+        </Box>
+      </TabPanel>
+      <TabPanel value={activeTab} index={1}>
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+          onSubmit={handleSubmit}
+        >
+          <TextField
+            autoFocus
+            margin="dense"
+            value={email}
+            onChange={handleChangeEmail}
+            id="email"
+            label={t('login.email')}
+            type="string"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            value={password1}
+            onChange={handleChangePassword1}
+            id="password1"
+            label={t('login.password')}
+            type="password"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            value={password2}
+            onChange={handleChangePassword2}
+            id="password2"
+            label={t('login.password')}
+            type="password"
+            fullWidth
+            variant="standard"
+          />
+          <div className="flex justify-between items-center mt-4">
+            <div>
+            <Button type="submit">{t('common.ok')}</Button>
+            <Button onClick={handleCancel}>{t('common.cancel')}</Button>
+            </div>
+            <FormControl sx={{ minWidth: 120 }} size="small">
+      <InputLabel id="demo-select-small-label">{t('common.language')}</InputLabel>
+      <Select
+        labelId="demo-select-small-label"
+        id="demo-select-small"
+        value={language}
+        label={t('common.language')}
+        onChange={handleLanguageChange}
+      >
+        <MenuItem value={'en'}>{t('common.english')}</MenuItem>
+        <MenuItem value={'de'}>{t('common.german')}</MenuItem>
+      </Select>
+    </FormControl>
+
+          </div>
+          <div className={styles.responseMsg}>
+            {[responseMsg].filter(value => !!value).map((value, index) =>
+              <span key={index}>{t('common.message')}: {value}</span>
+            )}
+          </div>
+        </Box>
+      </TabPanel>      
+    </div>);
 }
