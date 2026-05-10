21c0feeea3988a6461d3029af9952bd72a049fa3
Angular2guy <angular2guy@gmail.com>
2026-05-10 11:29:02 +0200

    fix: docker test

--- backend/buildDocker.sh
+++ backend/buildDocker.sh
@@ -4,7 +4,7 @@
 
 docker build -t angular2guy/timetracker-backend:latest --no-cache .
 
-docker run -p 8088:8088  -e PROD=8088 -e DB_USERNAME="sven1" -e DB_PASSWORD="sven1" -e DB_DATABASE="timetracker" --name timetracker-backend angular2guy/timetracker-backend:latest
+docker run -p 8088:8088  -e PORT=8088 -e DB_USERNAME="sven1" -e DB_PASSWORD="sven1" -e DB_DATABASE="timetracker" --name timetracker-backend angular2guy/timetracker-backend:latest
 
 docker start timetracker-backend
 docker stop timetracker-backend
