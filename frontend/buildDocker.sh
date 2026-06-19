1bc6bf82f39345b0ecf8285faae08277cfe1477e
Angular2Guy <angular2guy@gmail.com>
2026-06-19 19:27:32 +0200

    fix: docker

--- frontend/buildDocker.sh
+++ frontend/buildDocker.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # npm run build
-
+docker network create timetracker-network
 docker build -t angular2guy/timetracker-frontend:latest --no-cache .
 
-docker run -p 8080:80  -e SPRING_PROFILES_ACTIVE="prod"  --name timetracker angular2guy/timetracker-frontend:latest
+docker run --network=timetracker-network -p 8080:80  -e SPRING_PROFILES_ACTIVE="prod"  --name timetracker angular2guy/timetracker-frontend:latest
 
 docker start timetracker
 docker stop timetracker
