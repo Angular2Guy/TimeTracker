03dbbbc0d376c70fe39b68ac4ec65eb586f810d3
Angular2Guy <angular2guy@gmail.com>
2026-06-20 08:30:47 +0200

    fix: config

--- backend/buildDocker.sh
+++ backend/buildDocker.sh
@@ -6,7 +6,7 @@ docker network create timetracker-network
 
 docker build -t angular2guy/timetracker-backend:latest --no-cache .
 
-docker run --network=timetracker-network -p 8088:8088 -e PORT=8088 -e DB_USERNAME="sven1" -e DB_PASSWORD="sven1" -e DB_DATABASE="timetracker" --name timetracker-backend angular2guy/timetracker-backend:latest
+docker run --network=timetracker-network -p 8088:8088 -e PORT=8088 -e DB_HOST="timetracker-postgres" -e DB_USERNAME="sven1" -e DB_PASSWORD="sven1" -e DB_DATABASE="timetracker" --name timetracker-backend angular2guy/timetracker-backend:latest
 
 docker start timetracker-backend
 docker stop timetracker-backend
